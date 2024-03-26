4d1d71cd14c262c008bfd37f7240a1542af561fb
rangga <ranggantwleng77@gmail.com>
2024-03-26 20:09:29 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Rangga Arsy Prawira
+ Saya tinggal di daerah Depok
+ olah raga yang saya sukai adalah
+ Tenis Meja";
+ }
+}
